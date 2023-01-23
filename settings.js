e2a1c445d9dfb2c429204cd30acdf4b462c6f093
Bartek <99047458@mydavinci.nl>
2023-01-23 11:51:22 +0100

    Removed Change Theme function to setting.js

--- settings.js
+++ settings.js
@@ -17,3 +17,30 @@ function save(){
 	setTimeout("location.href = 'index.html';");
 }
 
+// Changes the theme of the game
+function changeTheme(){
+	if (localStorage.getItem("Theme") == "horizontal") {
+		alert("theme horizontal");
+		document.getElementById("divImg").classList.remove("col-6");
+		document.getElementById("divImg").classList.add("col-12");
+		document.getElementById("divBtn").classList.remove("col-6");
+		document.getElementById("divBtn").classList.add("col-12");
+
+		for (var i = 0; i <= 10; i++) {
+			document.getElementById("img-"+i).classList.add("col-2");
+			document.getElementById("img-"+i).classList.add("p-0");
+			document.getElementById("btn-"+i).classList.add("col-2");
+			document.getElementById("btn-"+i).classList.add("p-5");
+		}
+	} else if (localStorage.getItem("Theme") == "vertical"){
+		alert("theme vertical");
+		for (var i = 0; i <= 10; i++) {
+			document.getElementById("img-"+i).classList.add("col-6");
+			document.getElementById("img-"+i).classList.add("p-0");
+			document.getElementById("btn-"+i).classList.add("col-6");
+		}
+	}
+}
+
+changeTheme();
+
